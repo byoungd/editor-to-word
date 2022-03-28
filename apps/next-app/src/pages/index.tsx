0c57b6a5633a488a3db0fe544b8f9d203cb463b2
byoungd <pg98@vip.qq.com>
2022-03-28 10:28:55 +0800

    feat(next-app): update demo html content

--- apps/next-app/src/pages/index.tsx
+++ apps/next-app/src/pages/index.tsx
@@ -4,6 +4,10 @@ import { exportHtmlToDocx } from 'editor-to-word'
 import { useRef } from 'react'
 
 const Home = () => {
+  const demoHtml = `
+<p>Paste <span style="color: rgb(224, 62, 45);" data-mce-style="color: #e03e2d;">text</span> here or write them...</p><h2>Found a bug?</h2><p>If you think you have found a bug please create an issue on the GitHub repo to report it to the developers.</p><h2>Finally ...</h2><p><br data-mce-bogus="1"></p><p>Don't forget to check out our other product Plupload, your ultimate upload solution featuring HTML5 upload support.</p><p>Thanks for supporting TinyMCE! We hope it helps you and your users create great content.<br>All the best from the TinyMCE team.</p><p style="text-align: right;" data-mce-style="text-align: right;">2022.03.28</p>
+  `
+
   const editorRef = useRef<Editor['editor'] | null>(null)
   const handleDownload = () => {
     if (editorRef.current) {
@@ -11,6 +15,7 @@ const Home = () => {
       exportHtmlToDocx(html, 'test')
     }
   }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -22,8 +27,8 @@ const Home = () => {
         <div>TinyMce example:</div>
         <Editor
           apiKey="eatopd5mesqmfb7nto2utkbaf84mlgatef2df4h8nab4az89"
-          onInit={(evt, editor) => (editorRef.current = editor)}
-          initialValue="<p>Paste <span style='color: rgb(224, 62, 45);' data-mce-style='color: #e03e2d;'>text</span> here or write them...</p>"
+          onInit={(_, editor) => (editorRef.current = editor)}
+          initialValue={demoHtml}
           init={{
             height: 500,
             menubar: false,
